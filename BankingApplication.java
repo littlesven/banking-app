32c1e9416de25d17da564e3728e31ab408efcb8a
Stor <suzanna.stor@gmail.com>
2022-08-09 21:48:44 +0200

    Finished

--- BankingApplication.java
+++ BankingApplication.java
@@ -14,6 +14,12 @@ class BankAccount{
 	String customerName;
 	String customerID;
 	
+	// bank account constructor
+	BankAccount(String customerName, String customerID){
+		this.customerName = customerName;
+		this.customerID = customerID;
+	}
+	
 	void deposit(int amount) {
 		// Case 1 amount wanting to deposit is not 0
 		if(amount != 0) {
@@ -39,5 +45,52 @@ class BankAccount{
 		else {
 			System.out.println("None occured.");
 		}
+	}
+	
+	void showMenu() {
+		char option='\0';
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Welcome " + customerName);
+		System.out.println("Your ID: " + customerID);
+		System.out.println("\n");
+		System.out.println("A. Check balance");
+		System.out.println("B. Deposit");
+		System.out.println("C. Withdraw");
+		System.out.println("D. Check previous");
+		System.out.println("E. Exit");
+		
+		do {
+			System.out.println("Pick an option.");
+			option = scanner.next().charAt(0);
+			System.out.println("\n");
+			
+			switch(option) {
+			case 'A':
+				System.out.println("Balance: " + balance);
+				break;
+				
+			case 'B':
+				int amount = scanner.nextInt();
+				deposit(amount);
+				break;
+			
+			
+			case 'C':
+				int withdrawAmount = scanner.nextInt();
+				deposit(withdrawAmount);
+				break;
+			
+			case 'D':
+				getPreviousTransaction();
+			
+			case 'E':
+				break;
+				
+			default:
+				System.out.println("Please try again.");
+				break;
+			}
+		}while(option !='E');
 	}
 }
