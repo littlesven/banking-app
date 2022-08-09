6ecc1940cb58b45b77e308ca07216e2d1dd30ce6
Stor <suzanna.stor@gmail.com>
2022-08-09 21:21:20 +0200

    added banking application class

--- BankingApplication.java
+++ BankingApplication.java
@@ -1,1 +1,43 @@
+import java.util.Scanner;
 
+// Display Welcome message to the user
+// 5 functions: Check balance, deposit, withdraw, check previous, exit
+
+
+public class BankingApplication{
+	
+}
+
+class BankAccount{
+	int balance;
+	int previousTransaction;
+	String customerName;
+	String customerID;
+	
+	void deposit(int amount) {
+		// Case 1 amount wanting to deposit is not 0
+		if(amount != 0) {
+			balance = balance + amount;
+			previousTransaction = amount;
+		}
+	}
+	
+	void withdraw(int amount) {
+		if(amount != 0) {
+			balance = balance - amount;
+			previousTransaction = -amount;
+		}
+	}
+	
+	void getPreviousTransaction(){
+		if(previousTransaction > 0) {
+			System.out.println("Deposited: " + previousTransaction);
+		}
+		else if(previousTransaction < 0) {
+			System.out.println("Withdrawn: " + previousTransaction);
+		}
+		else {
+			System.out.println("None occured.");
+		}
+	}
+}
